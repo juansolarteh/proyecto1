ebdc166123a90384bff9e30cfe9b3cf8e34ec46c
FernandoCampoM <50385929+FernandoCampoM@users.noreply.github.com>
2022-03-09 15:40:24 -0500

    ok

--- proyecto1/PracticesBackend/src/main/java/com/practices/service/PracticesServiceAPI.java
+++ proyecto1/PracticesBackend/src/main/java/com/practices/service/PracticesServiceAPI.java
@@ -16,4 +16,5 @@ public interface PracticesServiceAPI extends GenericServiceAPI<Practices, Practi
 	public boolean addAttendees(String idResultado, String idStudent) throws Exception;
 	//public boolean addData(String idResultado, String variable, String value) throws Exception;
 	public String crearCSV(String idResultado) throws Exception;
+	
 }
